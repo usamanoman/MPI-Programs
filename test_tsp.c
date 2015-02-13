3f05a30048122bc40935423e5c6f70fd44defc02
Usama Noman <usama.noman@outlook.com>
2015-02-13 04:38:17 -0500

    TSP For 4 Nodes

--- test_tsp.c
+++ test_tsp.c
@@ -1,196 +1,178 @@
-#include <stdio.h>
+#include  <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
 #include <math.h>
 #include <time.h>
-
+#include<string.h>
 
 /* function to swap array elements */
 
 void swap (int v[], int i, int j) {
-  int t;
+int t;
 
-  t = v[i];
-  v[i] = v[j];
-  v[j] = t;
+t = v[i];
+v[i] = v[j];
+v[j] = t;
 }
 
 int find_min(int * array,int size)
 {
-    int c=0;
-    int minimum=array[c];
-    int location=0;
-    for ( c = 1 ; c < size ; c++ ) 
-    {
-        if ( array[c] < minimum ) 
-        {
-           minimum = array[c];
-           location = c;
-        }
-    } 
-    return location;
+int c=0;
+int minimum=array[c];
+int location=0;
+for ( c = 1 ; c < size ; c++ )
+{
+	if ( array[c] < minimum )
+	{
+		minimum = array[c];
+		location = c;
+	}
+}
+return location;
 }
 
 /* recursive function to generate permutations */
-void perm (int v[], int n, int i,int scores[],int graph[][4],int myid) {
+void perm (int v[], int n, int i,int scores[],int graph[][4],int myid,char * result[]) {
 
-  /* this function generates the permutations of the array
-   * from element i to element n-1
-   */
-   static int count=0;
-  int j;
-
-  /* if we are at the end of the array, we have one permutation
-   * we can use (here we print it; you could as easily hand the
-   * array off to some other function that uses it for something
-   */
-  if (i == n) {
-    for (j=0; j<n-1; j++){
-                printf ("%d,%d |", v[j],v[j+1]);
-                scores[count+(myid *6)]=scores[count+(myid *6)]+graph[v[j]][v[j+1]];
-          }
+/* this function generates the permutations of the array
+* from element i to element n-1
+*/
+static int count=0;
+int j;
+char buff[3];
 
 
-    printf ("(%d, %d)\n",scores[count+(myid *6)],myid);
-    count++;
-  } else
-    /* recursively explore the permutations starting
-     * at index i going through index n-1
-     */
-    for (j=i; j<n; j++) {
+/* if we are at the end of the array, we have one permutation
+* we can use (here we print it; you could as easily hand the
+* array off to some other function that uses it for something
+*/
+if (i == n) {
+	char *res;
+	res = (char *)malloc(sizeof(buff)+4+3);
+	strcpy(res,"");
+	for (j=0; j<n-1; j++){
+		snprintf(buff,sizeof(buff),"%d-",v[j]);
+		strcat(res,buff);
+		scores[count+(myid *6)]=scores[count+(myid *6)]+graph[v[j]][v[j+1]];
+	}
+	res[strlen(res)]=res[0];
+	result[count]=res;
+	//snprintf(result[count + (myid * 6 )],strlen(result[count+(myid*6)]),"%s", res);
+	//printf ("(%d, %d , %s)\n",scores[count+(myid *6)],myid,result[count]);
+	count++;
+} else
+/* recursively explore the permutations starting
+* at index i going through index n-1
+*/
+	for (j=i; j<n; j++) {
 
-      /* try the array with i and j switched */
+		/* try the array with i and j switched */
 
-      swap (v, i, j);
-      //printf("I was here");
-      perm (v, n, i+1,scores,graph,myid);
+		swap (v, i, j);
+		//printf("I was here");
+		perm (v, n, i+1,scores,graph,myid,result);
 
-      /* swap them back the way they were */
+		/* swap them back the way they were */
 
-      swap (v, i, j);
-    }
+		swap (v, i, j);
+	}
 }
 
 /* little driver function to print perms of first 5 integers */
- 
+
 /************************************************************
 This is a simple send/receive program in MPI
 ************************************************************/
-struct astruct{ 
-    int value; 
-    int   index; 
-} ; 
-
-
-
 
 int main(argc,argv)
 int argc;
 char *argv[];
 {
-    clock_t begin, end;
-    double time_spent;
-    struct astruct in[1];
-    struct astruct out[1];
-    int myid, numprocs;
-    int tag,source,destination,count;
-    int buffer=0;
-    MPI_Status status;
- 
-    MPI_Init(&argc,&argv);
-    MPI_Comm_size(MPI_COMM_WORLD,&numprocs);
-    MPI_Comm_rank(MPI_COMM_WORLD,&myid);
-    tag=1234;
-    source=0;
-    destination=1;
-    count=1;
-    int v[4], i,j;
-    int minimum;
-    int allmin;
-    int location=0;
-    int mini[2]={65,65};
-    int allmini[2]={65,65};
-    int allLocation[4];
-    int graph[4][4]={
-        {0,2,9,2},
-        {3,0,5,3},
-        {2,4,0,2},
-        {5,6,5,0}
-    };
-    for (i=0; i<4; i++) v[i] = i;
-    int scores[24];
-    for(i=0;i<24;i++){
-        scores[i]=0;
-  
-    }
+	clock_t begin, end;
+	double time_spent;
+	int myid, numprocs;
+	int tag,source,destination,count;
+	int buffer=0;
+	MPI_Status status;
+	
+	MPI_Init(&argc,&argv);
+	MPI_Comm_size(MPI_COMM_WORLD,&numprocs);
+	MPI_Comm_rank(MPI_COMM_WORLD,&myid);
+	tag=1234;
+	source=0;
+	destination=1;
+	count=1;
+	int v[4], i,j;
+	int location=0;
+	int mini_with_rank[2]={65,65};
+	int allmini_with_rank[2]={65,65};
+	int mini_with_loc[2]={65,65};
+	int allmini_with_loc[2]={65,65};
+	int allLocation[4];
+	int graph[4][4]={
+	{0,2,9,2},
+	{3,0,5,3},
+	{2,4,0,2},
+	{5,6,5,0}
+	};
+	for (i=0; i<4; i++) v[i] = i;
+	int scores[24];
+	for(i=0;i<24;i++){
+		scores[i]=0;
+	}
+	char * result[6];
+	if(myid == source){
+		printf("The graph is:\n");
+		for(i=0;i<4;i++){
+		    for(j=0;j<4;j++)
+			printf("%d ,",graph[i][j]);
+		    printf("\n");
+		}
 
+		begin = clock();
+		printf("Enter a number:");
+		scanf("%d",&buffer);
+		for(i = 1 ; i < numprocs ; i++){
+			MPI_Send(&scores[(i-1)*6],6,MPI_INT,i,tag+1,MPI_COMM_WORLD);
+			MPI_Send(&buffer,count,MPI_INT,i,tag,MPI_COMM_WORLD);
+		}
+		for(i = 1 ; i < numprocs ; i++){
+			MPI_Recv(&scores[(i-1)*6],6,MPI_INT,i,tag+2,MPI_COMM_WORLD,&status);
+		}
+		MPI_Barrier(MPI_COMM_WORLD);
+		
+	}
+	if(myid != 0){
+		MPI_Recv(&scores[(myid-1) * 6],6,MPI_INT,source,tag+1,MPI_COMM_WORLD,&status);
+		MPI_Recv(&buffer,count,MPI_INT,source,tag,MPI_COMM_WORLD,&status);
 
-    if(myid == source){
-      
-      mini[0]=15;
-      mini[1]=myid;
-      begin = clock();
-      scores[1]++;
-      printf("Enter a number:");
-      scanf("%d",&buffer);
-      for(i = 1 ; i < numprocs ; i++){
-          MPI_Send(&scores[(i-1)*6],6,MPI_INT,i,tag+1,MPI_COMM_WORLD);
-          MPI_Send(&buffer,count,MPI_INT,i,tag,MPI_COMM_WORLD);
-      }
-      printf("processor %d  sent %d\n",myid,buffer);
-      for(i = 1 ; i < numprocs ; i++){
-  MPI_Recv(&scores[(i-1)*6],6,MPI_INT,i,tag+2,MPI_COMM_WORLD,&status);
-      //printf("Value: %d and location %d\n",allmin[0],allmin[1]);
-      //MPI_Barrier(MPI_COMM_WORLD);
-  }
-      MPI_Barrier(MPI_COMM_WORLD);
-      MPI_Reduce( &minimum, &allmin, 1, MPI_INT, MPI_MIN, 0, MPI_COMM_WORLD );
-      printf("Value : %d, and location %d\n",allmin, myid);
-      //for(i=1;i<numprocs;i++)
-  //MPI_Recv(&location,1,MPI_INT, 4,tag+3,MPI_COMM_WORLD, &status);
-        
-    }
-    if(myid != 0){
-        MPI_Recv(&scores[(myid-1) * 6],6,MPI_INT,source,tag+1,MPI_COMM_WORLD,&status);
-        MPI_Recv(&buffer,count,MPI_INT,source,tag,MPI_COMM_WORLD,&status);
-        swap(v,0,myid-1);
-        perm (v, buffer, 1,scores,graph,myid-1);
-        location=find_min(&scores[(myid-1)*6],6);
-  in[0].value = scores[((myid - 1 )*6) + location  ];
-  in[0].index = location ;
-  minimum = scores[((myid - 1 )*6) + location  ];
-  mini[0] = scores[((myid - 1 ) * 6 ) + location];
-        mini[1] =( (myid - 1 ) * 6) +  location;
-  printf("Value : %d and my rank is %d\n",minimum,myid);
-  
-  MPI_Send(&scores[(myid-1)*6],6,MPI_INT,0,tag+2,MPI_COMM_WORLD);
-  MPI_Barrier(MPI_COMM_WORLD);
-  MPI_Reduce( &minimum, &allmin, 1, MPI_INT, MPI_MIN, 0, MPI_COMM_WORLD );
-  
-  //MPI_Allreduce(minimum, allmin, 1, MPI_2INT, MPI_MINLOC, MPI_COMM_WORLD);
-//  if(myid ==1)
-//  printf("WOrld");
-//  MPI_Send(&location, 1 , MPI_INT, 0 , tag +3 , MPI_COMM_WORLD);
-    }
-    
-    MPI_Barrier(MPI_COMM_WORLD);      
-  //  if(myid == 1)
-    MPI_Allreduce(mini,allmini,1 , MPI_2INT,MPI_MINLOC,MPI_COMM_WORLD);
-    if(myid != source)
-      printf("Value : %d , the process: %d\n" , allmini[0],allmini[1]);
-        
-
-
-    if(myid == source)
-    {
-        for(i=0;i<24;i++){
-            printf("%d,",scores[i]);
-        }
-  printf("\n");
-        end = clock();
-        time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-        printf("%f\n",time_spent);
-    }
-    MPI_Finalize();
-    return 0;
+		swap(v,0,myid-1);
+		perm (v, buffer, 1,scores,graph,myid-1,result);
+		location=find_min(&scores[(myid-1)*6],6);
+		mini_with_loc[0] = scores[((myid - 1 ) * 6 ) + location];
+		mini_with_loc[1] =location;
+		mini_with_rank[0] = scores[((myid - 1 ) * 6 ) + location];
+		mini_with_rank[1] =myid;
+		MPI_Send(&scores[(myid-1)*6],6,MPI_INT,0,tag+2,MPI_COMM_WORLD);
+		MPI_Barrier(MPI_COMM_WORLD);
+		
+	}
+	
+	MPI_Barrier(MPI_COMM_WORLD);
+	MPI_Allreduce(mini_with_rank,allmini_with_rank,1 , MPI_2INT,MPI_MINLOC,MPI_COMM_WORLD);
+	MPI_Allreduce(mini_with_loc,allmini_with_loc,1 , MPI_2INT,MPI_MINLOC,MPI_COMM_WORLD);
+	if(myid == allmini_with_rank[1]){
+		MPI_Send(result[allmini_with_loc[1]],strlen(result[allmini_with_loc[1]])+1,MPI_CHAR,0,tag+7,MPI_COMM_WORLD);	
+		printf("My rank is : %d, I have found a minimum path:%s with value:%d\n",allmini_with_rank[1],result[allmini_with_loc[1]],allmini_with_loc[0]);
+	}
+	MPI_Barrier(MPI_COMM_WORLD);	
+	
+	if(myid == source)
+	{
+		end = clock();
+		time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+		printf("%f\n",time_spent);
+	}
+	MPI_Finalize();
+	return 0;
 }
